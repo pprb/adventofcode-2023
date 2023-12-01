9731ad30866472d07128b8aae7829f71fc9116cf
PPRB <loic@beebs.app>
2023-12-01 19:24:30 +0100

    day 1 - part 2

--- src/day1/index.js
+++ src/day1/index.js
@@ -3,7 +3,7 @@ const readline = require('readline');
 
 const re = /\d/g;
 
-(async function day1() {
+async function day1part1() {
   let result = 0;
 
   const lineReader = readline.createInterface({
@@ -20,6 +20,57 @@ const re = /\d/g;
 
   lineReader.on('close', () => {
     console.log('file closed');
+    console.log(`result: ${result}`);
+  });
+}
+
+(async function day1part2() {
+  let result = 0;
+
+  const numbersString = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+  const lineReader = readline.createInterface({
+    input: fs.createReadStream('./input.txt'),
+    crlfDelay: Infinity,
+  });
+
+  lineReader.on('line', (line) => {
+    console.log(`Line: ${line}`);
+    let lineNumbers = '';
+    let stringBuffer = '';
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const c of line) {
+      // console.log(stringBuffer);
+      // number -> add to lineNumbers
+      if (!isNaN(c)) {
+        lineNumbers += c;
+      } else {
+        // add to stringBuffer
+        stringBuffer += c;
+        // console.log(stringBuffer);
+        // search if the string buffer is the start of a number
+        if (numbersString.some((n) => n.startsWith(stringBuffer))) {
+          const pos = numbersString.indexOf(stringBuffer);
+          if (pos !== -1) {
+            lineNumbers += pos + 1;
+            stringBuffer = c;
+          }
+        } else {
+          // clean previous car
+          stringBuffer = c;
+        }
+      }
+    }
+
+    console.log(`Line cleaned: ${lineNumbers}`);
+    console.log(lineNumbers[0] + lineNumbers[lineNumbers.length - 1]);
+    const value = parseInt(lineNumbers[0] + lineNumbers[lineNumbers.length - 1], 10);
+    result += value;
+  });
+
+  lineReader.on('close', () => {
+    // console.log('file closed');
     console.log(`result: ${result}`);
   });
 }());
